0bb2e6ae1a0b80119520c661bd4b98e782ff9b85
Kamil Paduszyński <kpaduszy@gmail.com>
2022-06-03 00:47:34 +0200

    Start and install the employees app

--- src/project/urls.py
+++ src/project/urls.py
@@ -4,4 +4,5 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("units/", include("units.urls")),
+    path("employees/", include("employees.urls")),
 ]
