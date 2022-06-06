f2af5761f861e4ad56bb1eef53aea6d5561c68a9
Kamil Paduszyński <kpaduszy@gmail.com>
2022-06-06 16:06:19 +0200

    Add user icon to the User model admin list view

--- src/accounts/admin.py
+++ src/accounts/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.template.loader import render_to_string
+from django.utils.translation import gettext_lazy as _
 
 User = get_user_model()
 
@@ -31,9 +33,23 @@ class UserAdmin(BaseUserAdmin):
         "username",
         "email",
         "first_name",
+        "get_user_icon",
         "last_name",
         "is_staff",
         "has_employee",
     )
     list_display_links = ("id",)
     ordering = ("id",)
+
+    @admin.display(description=_("Zdjęcie"))
+    def get_user_icon(self, obj):
+        """Return HTML of the user's photo."""
+        if obj.icon:
+            attrs = {
+                "src": obj.icon.url,
+                "alt": f'{_("Zdjęcie użytkownika")}: {obj.get_full_name()}',
+            }
+            return render_to_string(
+                template_name="snippets/tag.html",
+                context={"name": "img", "attrs": attrs},
+            )
