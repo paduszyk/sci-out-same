db6ea535fed97c0e7754fd9efdb91267368c2d58
Kamil Paduszyński <kpaduszy@gmail.com>
2022-06-06 15:47:50 +0200

    Configure media URLs

--- src/project/urls.py
+++ src/project/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
@@ -12,3 +14,9 @@ urlpatterns = [
     path("authorships/", include("authorships.urls")),
     path("articles/", include("articles.urls")),
 ]
+
+urlpatterns = (
+    urlpatterns
+    + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+)
