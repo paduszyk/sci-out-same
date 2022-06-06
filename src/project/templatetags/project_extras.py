802c65e6d9661c04e6c63f9db36d1f568ba37e42
Kamil Paduszyński <kpaduszy@gmail.com>
2022-06-06 16:05:06 +0200

    Define templatetag for rendering HTML tags and apply it to CSS links

--- src/project/templatetags/project_extras.py
+++ src/project/templatetags/project_extras.py
@@ -1,3 +1,28 @@
 from django import template
+from django.templatetags.static import static
 
 register = template.Library()
+
+
+@register.inclusion_tag("snippets/tag.html", name="tag")
+def get_html_tag(name, closing_tag=False, content=None, **attrs):
+    """Return HTML code for rendering a tag."""
+    return {
+        "name": name,
+        "closing_tag": closing_tag,
+        "content": content,
+        "attrs": attrs,
+    }
+
+
+@register.inclusion_tag("snippets/tag.html", name="css_link")
+def get_css_link_html_tag(href, **attrs):
+    """Return HTML for rendering CSS link."""
+    attrs.update(
+        {
+            "rel": "stylesheet",
+            "type": "text/css",
+            "href": static(href),
+        }
+    )
+    return get_html_tag("link", closing_tag=False, content=None, **attrs)
