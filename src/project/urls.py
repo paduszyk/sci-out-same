af23e71050ecb60549c2a883f7c1a59139131372
Kamil Paduszyński <kpaduszy@gmail.com>
2022-06-08 17:54:18 +0200

    Update admin site titles and project's URLs

--- src/project/urls.py
+++ src/project/urls.py
@@ -1,5 +1,8 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from django.utils.translation import gettext_lazy as _
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -8,3 +11,13 @@ urlpatterns = [
     # Project apps URLs
     path("units/", include("units.urls")),
 ]
+
+urlpatterns = (
+    urlpatterns
+    + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+)
+
+admin.site.site_header = _("Baza dorobku pracowników")
+admin.site.site_title = _("BDP")
+admin.site.index_title = _("Administracja")
